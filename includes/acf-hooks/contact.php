fd126f210edd092fc233807089909cba556ac1ce
Brian <brian@brianaderer.com>
2023-11-18 11:44:42 -0500

    create form ACF

---
+++ includes/acf-hooks/contact.php
@@ -0,0 +1,11 @@
+<?php
+add_action('acf/init', 'contact_block');
+function contact_block(): void {
+	if (function_exists('acf_register_block_type')) {
+	acf_register_block_type(array(
+		'name'              => 'transient-contact-form',
+		'title'             => __('Transient Email Form'),
+		'description'       => __('Form for Transient Submissions'),
+	));
+	}
+}
