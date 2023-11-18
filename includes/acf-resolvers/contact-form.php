56c8a48e94977272c434b8c51fe604c371272711
Brian <brian@brianaderer.com>
2023-11-18 13:20:12 -0500

    expose custom ACF data for Transient Form to GQL

---
+++ includes/acf-resolvers/contact-form.php
@@ -0,0 +1,36 @@
+<?php
+function resolve_acf_group_fields():array {
+	logger('called');
+	$field_group_key = "group_6558e578f3a2b"; // Replace with your field group key
+	$fields = acf_get_fields( $field_group_key );
+	$result = [];
+			foreach ( $fields as $field ) {
+				$choices = null;
+				if( array_key_exists( 'choices', $field ) ):
+					$choices = $field['choices'];
+				endif;
+			$result[ $field['name'] ] = array(
+				'label' => $field['label'],
+				'value' => $field['value'],
+				'options' => $choices,
+				'type'  => $field['type'],
+				'placeholder' => $field['placeholder'],
+			);
+		}
+return $result;
+}
+add_action( 'graphql_register_types', function() {
+	$result = resolve_acf_group_fields();
+	try{
+	register_graphql_field( 'AcfTransientContactForm', 'contactData', [
+		'type' => 'String',
+		'description' => __( 'Expose ACF Generated Contact Form in the GQL Schema', 'replace-with-your-textdomain' ),
+		'resolve' => function( $root, $args, $context, $info ) {
+			return json_encode( resolve_acf_group_fields() );
+		}
+	] );
+	}
+	catch (Exception $e){
+		logger( $e );
+	}
+});
