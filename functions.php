bda878471f00727877ba9f1e01c1178176e49db4
Brian <brian@brianaderer.com>
2023-11-18 09:13:39 -0500

    beginning custom logging, refactor to includes structure

--- functions.php
+++ functions.php
@@ -1,20 +1,7 @@
 <?php
-add_action('acf/init', 'my_acf_init_block_types');
-function my_acf_init_block_types() {
 
-	// Check function exists.
-	if( function_exists('acf_register_block_type') ) {
-
-		// register a testimonial block.
-		acf_register_block_type(array(
-			'name'              => 'Reviews',
-			'title'             => __('Reviews'),
-			'description'       => __('A custom testimonial block.'),
-			//'render_template'   => 'template-parts/blocks/testimonial/testimonial.php',
-			'category'          => 'formatting',
-			'icon'              => 'admin-comments',
-			'keywords'          => array( 'testimonial', 'quote' ),
-		));
-	}
-
+// Enable Debug logging to the /wp-content/debug.log file
+define( 'WP_DEBUG_LOG', './wp-content/themes/HMB/logs/debug.log' );
+function logger( mixed $input ): void {
+	error_log( print_r( $input, true ) );
 }
