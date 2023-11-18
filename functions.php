93dfa70d44ad3ca6670ead5d3697411364d5739a
Brian <brian@brianaderer.com>
2023-11-18 09:19:13 -0500

    logging set up

--- functions.php
+++ functions.php
@@ -1,7 +1,23 @@
 <?php
 
-// Enable Debug logging to the /wp-content/debug.log file
-define( 'WP_DEBUG_LOG', './wp-content/themes/HMB/logs/debug.log' );
+add_action('acf/init', 'reviews_block');
+function reviews_block(): void {
+
+	// Check function exists.
+	if( function_exists('acf_register_block_type') ) {
+
+		// register a testimonial block.
+		acf_register_block_type(array(
+			'name'              => 'Reviews',
+			'title'             => __('Reviews'),
+			'description'       => __('A custom testimonial block.'),
+			'category'          => 'formatting',
+			'icon'              => 'admin-comments',
+			'keywords'          => array( 'reviews'),
+		));
+	}
+
+}
 function logger( mixed $input ): void {
 	error_log( print_r( $input, true ) );
 }
--- includes/acf-hooks/reviews.php
+++ includes/acf-hooks/reviews.php
@@ -1,19 +1,1 @@
 <?php
-add_action('acf/init', 'reviews_block');
-function reviews_block() {
-
-	// Check function exists.
-	if( function_exists('acf_register_block_type') ) {
-
-		// register a testimonial block.
-		acf_register_block_type(array(
-			'name'              => 'Reviews',
-			'title'             => __('Reviews'),
-			'description'       => __('A custom testimonial block.'),
-			'category'          => 'formatting',
-			'icon'              => 'admin-comments',
-			'keywords'          => array( 'reviews'),
-		));
-	}
-
-}
