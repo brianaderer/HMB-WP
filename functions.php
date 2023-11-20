413f85640a13bd915ef06262ad79cabd6901e3f9
Brian <brian@brianaderer.com>
2023-11-19 21:37:58 -0500

    automatic enqueue working

--- functions.php
+++ functions.php
@@ -6,9 +6,10 @@ function logger( mixed $input ): void {
 
 //now include all the rest of our code
 
+//composer files. theme isn't working? try running composer install?
 require_once( 'vendor/autoload.php' );
 require_once( 'test/test.php' );
-require_once( 'dynamic/dynamic.php' );
+
 $dotenv = Dotenv\Dotenv::createImmutable( __DIR__ );
 $dotenv->load();
 
@@ -18,6 +19,18 @@ function return_dirs( $path ): array {
 function return_files( $path ): array {
 	return glob( $path );
 }
+
+$blocks_path =  trailingslashit( get_stylesheet_directory() ) . 'blocks/*';
+$blocks_dirs = return_dirs( $blocks_path );
+
+foreach( $blocks_dirs as $blocks_dir ):
+		$array = explode( '/', $blocks_dir );
+		$slug = array_pop( $array );
+		$path = $blocks_dir . '/' . $slug . '.php';
+		if( file_exists( $path ) ):
+				require_once( $path );
+			endif;
+	endforeach;
 
 $path =  trailingslashit( get_stylesheet_directory() ) . 'includes/*' ;
 $admin_path =  trailingslashit( get_stylesheet_directory() ) . 'admin_includes/*' ;
@@ -39,4 +52,5 @@ enqueue_includes( $path );
 if( is_admin() ):
 	enqueue_includes( $admin_path );
 endif;
+
 //Pencils Down. No more Edits to the Functions file. Make an Include.
