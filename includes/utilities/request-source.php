ab3916813ce6c51f5ddb03a513a0b827694aa669
Brian <brian@brianaderer.com>
2023-11-22 04:42:20 -0500

    dont need the save form hook either

--- includes/utilities/request-source.php
+++ includes/utilities/request-source.php
@@ -8,10 +8,3 @@ function is_admin_request(): bool {
 	 return str_contains( $_SERVER['HTTP_REFERER'], get_admin_url() );
 }
 
-add_filter('acf/pre_save_post', 'my_acf_pre_save_post', 10, 2);
-function my_acf_pre_save_post( $post_id, $form ) {
-	logger( $form );
-	// Create post using $form and update $post_id.
-	return $post_id;
-}
-
