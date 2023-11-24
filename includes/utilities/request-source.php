aeccaeec745cfe894cb987d41b705082c906813f
Brian <brian@brianaderer.com>
2023-11-24 16:41:00 -0500

    remove debug

--- includes/utilities/request-source.php
+++ includes/utilities/request-source.php
@@ -1,7 +1,6 @@
 <?php
 function is_rest_api_request(): bool {
 	$rest_prefix = trailingslashit( rest_get_url_prefix() );
-	logger($_SERVER['REQUEST_URI']);
 	return ( str_contains( $_SERVER['REQUEST_URI'], $rest_prefix ) );
 }
 
