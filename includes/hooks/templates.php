96893c695ae824caccdec602f99c49001cb772e0
Brian <brian@brianaderer.com>
2023-11-26 20:05:13 -0500

    update field groups

--- includes/hooks/templates.php
+++ includes/hooks/templates.php
@@ -2,5 +2,6 @@
 add_filter('theme_page_templates', 'register_custom_page_templates');
 function register_custom_page_templates($templates) {
 	$templates['attractions'] = 'Attractions';
+	$templates['guest_book'] = 'Guest Book';
 	return $templates;
 }
